cfcb48bede27bef6e8668715d3feff49e17f03bc
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-06-02 22:50:33 +0900

    Update GoogleDriveTest.py

--- project/test/GoogleDriveTest.py
+++ project/test/GoogleDriveTest.py
@@ -6,18 +6,18 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+SCOPES = ["https://www.googleapis.com/auth/drive"] # scopes 수정시 token.pickle 지워야함
 
 
-def file_upload():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
+def file_upload(type):
+    """
+    구글Drive v3 API
+    음성 or 이미지 업로드하는 함수
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # created automatically when the authorization flow completes for the first time.
     if os.path.exists("token.pickle"):
         with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
@@ -26,40 +26,44 @@ def file_upload():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            # credentials 저장
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
-        # credentials 저장
+
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
     service = build("drive", "v3", credentials=creds)
 
-    # Call the Drive v3 API
-    results = (
-        service.files()
-        .list(
-            pageSize=20,
-            fields="nextPageToken, files(id, name)",
-        )
-        .execute()
-    )
-    items = results.get("files", [])
+    # Call the Drive v3 API 테스트 코드
+    # results = (
+    #     service.files()
+    #     .list(
+    #         pageSize=20,
+    #         fields="nextPageToken, files(id, name)",
+    #     )
+    #     .execute()
+    # )
+    # items = results.get("files", [])
 
-    if not items:
-        print("No files found.")
-    else:
-        print("Files:")
-        for item in items:
-            print(u"{0} ({1})".format(item["name"], item["id"]))
+    # if not items:
+    #     print("No files found.")
+    # else:
+    #     print("Files:")
+    #     for item in items:
+    #         print(u"{0} ({1})".format(item["name"], item["id"]))
 
 
-    # 이미지 저장 (.jpg)
-    file_metadata = {"name": "photo.jpg", "parents": ["1aAdyOkkIrwiGr-Bqy_MOS5xbNHwjCvaR"]} # 저장명, 저장폴더id
-    media = MediaFileUpload("C:/Dev/Github/Smart_Bell/project/test/cat2.jpg", mimetype="image/jpeg")    # 업로드할 파일 경로 및 타입
+    if type=="image":
+        # 이미지 저장 (.jpg)
+        file_metadata = {"name": "test.jpg", "parents": ["1aAdyOkkIrwiGr-Bqy_MOS5xbNHwjCvaR"]} # 저장명, 저장폴더id
+        media = MediaFileUpload("C:/Dev/Github/Smart_Bell/project/test/test.jpg", mimetype="image/jpeg")    # 업로드할 파일 경로 및 타입
+        
+    elif type=="wav":
+        # 녹음 저장 (.wav)
+        file_metadata = {"name": "test.wav", "parents": ["1aAdyOkkIrwiGr-Bqy_MOS5xbNHwjCvaR"]}
+        media = MediaFileUpload("C:/Dev/Github/Smart_Bell/project/test/test.wav", mimetype="media/wav")
     file = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
-    print(file)
-
-    # 녹음 저장 (.wav)
 
 
 if __name__ == "__main__":
